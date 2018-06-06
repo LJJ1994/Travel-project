e3b2628f0c1c5a456e13d8a6da9b948a838eb07f
LJJ <LJJLOVEU@yeah.net>
2018-06-06 18:45:26 +0800

    首页轮播图制作

--- README.md
+++ README.md
@@ -17,11 +17,53 @@ npm run build
 # build for production and view the bundle analyzer report
 npm run build --report
 
+
+
+
+
+
+
 一、制作首页的内容header
+
 1.安装stylus 和stylus loader  css预编译语言
+
 1rem = html font-size 50px
+
 2.利用stylus自定义css变量，以达到优化css的目的
+
 3.修改webpack的路径配置，减少路径的复杂使用，然后重启服务器
+
+
+
+
+二、首页轮播图
+
+1.借助轮播插件vue-awesome-swiper
+
+在https://github.com/surmon-china/vue-awesome-swiper下
+
+npm install vue-awesome-swiper@2.6.7 --save
+
+2.问题：在轮播途中，给轮播图的灰色点号加css样式报错
+
+	.wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+
+3.用vuejs中的v-for对轮播图进行循环输出，在返回的data中的swiperOption，利用loop属性，设置为true，可以进行左右循环输出。
+
+GIT合并分支工作流程：
+
+git add .
+
+git commit -m '轮播图制作'
+
+git push
+
+git checkout master
+
+git merge origin/index-swiper
+
+git push
 
 
 
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,48 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+	    <swiper-slide v-for="item of swiperList" :key="item.id">
+	      <img 
+	      class="swiper-img"
+	      :src="item.imgUrl"
+	      >
+	    </swiper-slide>
+	    <div class="swiper-pagination"  slot="pagination"></div>
+	  </swiper>
+	</div>
+</template>
+
+<script type="text/javascript">
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+      	pagination: '.swiper-pagination',
+      	loop: true
+      },
+      swiperList: [{
+      	id: '001',
+      	imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3b/ef86879aa50e3002.jpg_750x200_2a108508.jpg'
+      },{
+      	id: '002',
+      	imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'
+      },{
+      	id: '003',
+      	imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+  	overflow: hidden
+  	width: 100%
+  	height:0
+  	padding-bottom: 31.25%
+  	background: #eee
+	  .swiper-img
+	    width: 100%
+</style>
