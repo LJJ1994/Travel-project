6df027165fa8a2234f6cf73e85994e089cb89f82
LJJ <LJJLOVEU@yeah.net>
2018-06-07 19:10:10 +0800

    城市路由

---
+++ src/pages/city/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="header">
+		<router-link to='/'>
+			<div class="iconfont back-header">&#xe624;</div>
+		</router-link>
+		城市选择
+	</div>
+</template>
+
+<script type="text/javascript">
+export default {
+	name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varible.styl'
+	.header
+		position: relative
+		overflow: hidden
+		height: $headerHeight
+		line-height: $headerHeight
+		text-align: center
+		background: #804e9a
+		color: #fff
+		font-size: .32rem
+		.back-header
+			position: absolute
+			width: .64rem
+			font-size: .4rem
+			text-align: center
+			color: #fff
+</style>
--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -6,9 +6,11 @@
     <div class="header-center">
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题</div>
-    <div class="header-right">
-      {{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to='/city'>
+      <div class="header-right">
+        {{this.city}}<span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 
@@ -47,6 +49,7 @@ export default{
       width: 1.24rem
       float: right
       text-align: center
+      color: #eee
       .arrow-icon
         font-size: .2rem
         padding-left: .1rem
